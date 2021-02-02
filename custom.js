ecaddbae33a2e91999e330475f0beba6a6edad19
webbush20 <frontendbush@gmail.com>
2021-02-02 07:14:09 -0800

    null vs undifine

---
+++ custom.js
@@ -0,0 +1,14 @@
+//let pakhi
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+// function add(num1, num2) {
+//     console.log(num1, num2)
+// }
+// const premik = {name:'guljar ahmed', phone:626862,}
+// console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+let ages = [2, 5, 9];
+console.log(ages[11]);
