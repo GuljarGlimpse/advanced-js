20dca38162e0325a47ee799895cf3426f94bb523
webbush20 <frontendbush@gmail.com>
2021-02-02 13:24:56 -0800

    another 2 update

---
+++ arrysplite.js
@@ -0,0 +1,8 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const part = nums.slice(1, 5);
+
+const removed = nums.splice(2,5, 55);
+// console.log(removed);
+// console.log(nums);
+const together = nums.join("ami ");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus = 20;
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    if (result > 9) {
+        var mood = "happy";
+        mood = "sad";
+        mood = "glow";
+        
+    }
+    var day = "friday"
+    console.log(day);
+    return result;
+
+}
+const output = sum(3, 7);
+console.log(output);
